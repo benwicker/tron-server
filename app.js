ca8117f70fc70ff441c6718de875e24f04c1df02
Ben Wicker <bwicker@cedarville.edu>
2019-05-13 18:10:24 -0400

    trying to update server

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const WebSocket = require('ws');
+const Player = require('./player');
+import { Server } from 'ws';
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new Server({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
+    ws.send('echo: ' + message);
   });
 
-  ws.send('something');
+  let player = new Player(0, 'red');
+
+  ws.send(player);
 });
---
+++ player.js
@@ -0,0 +1,8 @@
+class Player {
+    constructor(id, color) {
+        this.id = id;
+        this.color = color;
+    }
+}
+
+module.exports = Player;
